ec7259203bbaef0da161666230e352bf0d29e76a
Your Name <your.email@example.com>
2025-02-20 18:16:00 +0300

    updaste code host

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ print(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 # Application definition
